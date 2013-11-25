383dffc6e3e7a2103fc48b7813b751aacf50d4d6
Alexander.Getmansky <getmansk-y@yandex.ru>
2013-11-25 11:48:02 +0400

    Added configure for unit test

---
+++ tests/GetSky/Phalcon/AutoloadServices/Tests/RegistrantTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace GetSky\Phalcon\AutoloadServices\Tests;
+
+//use Phalcon\Test\UnitTestCase as PhalconTestCase;
+
+use Phalcon\Config;
+
+class RegistrantTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testIsInjectionAwareInterface()
+    {
+    }
+
+} 
