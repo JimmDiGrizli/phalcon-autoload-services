8ac36134326dcc9f00c6bdb8e7cc9f47f24d3af1
Alexander.Getmansky <getmansk-y@yandex.ru>
2013-11-26 11:22:49 +0400

    Edited RegistrantTest

--- tests/GetSky/Phalcon/AutoloadServices/Tests/RegistrantTest.php
+++ tests/GetSky/Phalcon/AutoloadServices/Tests/RegistrantTest.php
@@ -3,18 +3,53 @@ namespace GetSky\Phalcon\AutoloadServices\Tests;
 
 use GetSky\Phalcon\AutoloadServices\Registrant;
 use Phalcon\Config;
+use Phalcon\Config\Adapter\Ini;
 
 class RegistrantTest extends \PHPUnit_Framework_TestCase
 {
 
+    protected $services;
+
+    protected $servicesTwo;
+
+    /**
+     * @var Registrant
+     */
+    protected $registrant;
+
+    protected function setUp()
+    {
+        $this->services = new Ini('service.ini');
+        $this->servicesTwo = new Ini('serviceTwo.ini');
+        $this->registrant = new Registrant($this->services);
+    }
+
     public function testIsInjectionAwareInterface()
     {
-        $config = new Config(array());
-
         $this->assertInstanceOf(
             'Phalcon\DI\InjectionAwareInterface',
-            new Registrant($config)
+            $this->registrant
         );
+    }
+
+    public function testGetService()
+    {
+        $service = $this->registrant->getServices();
+        $this->assertObjectHasAttribute('route', $service);
+    }
+
+    public function testSetService()
+    {
+        $this->registrant->setServices($this->servicesTwo);
+        $service = $this->registrant->getServices();
+        $this->assertObjectHasAttribute('routeTwo', $service);
+    }
+
+    protected function tearDown()
+    {
+        $this->services = null;
+        $this->servicesTwo = null;
+        $this->registrant = null;
     }
 
 } 
