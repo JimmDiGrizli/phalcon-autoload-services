b609b7ced6afcc9ac27905854bc18eb046f6c3a3
Alexander.Getmansky <getmansk-y@yandex.ru>
2013-12-09 14:04:06 +0400

    Update ProviderCreator

--- tests/GetSky/Phalcon/AutoloadServices/Tests/Creators/ProviderCreatorTest.php
+++ tests/GetSky/Phalcon/AutoloadServices/Tests/Creators/ProviderCreatorTest.php
@@ -6,6 +6,25 @@ use Phalcon\Config;
 
 class ProviderCreatorTest extends CreatorTest
 {
+    public function testCreator()
+    {
+        $config = new Config(
+            array(
+                'provider' => 'RouteProvider'
+            )
+        );
+        $this->creator = new ProviderCreator($this->di, $config);
+        $this->di->set('testProviderCreator', $this->creator->injection());
+        $this->assertInstanceOf(
+            'Phalcon\Mvc\Router',
+            $this->di->get('testProviderCreator')
+        );
+
+        $config = new Config(array('provider' => '%off%'));
+        $this->creator = new ProviderCreator($this->di, $config);
+        $this->assertNull($this->creator->injection());
+    }
+
     /**
      * @expectedException \GetSky\Phalcon\AutoloadServices\Creators\Exception\ClassNotFoundException
      */
