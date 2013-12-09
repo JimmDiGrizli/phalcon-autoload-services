cccd1d2e3e8ac8ba7aad4cf8f6bbbbb8bc16fce9
Alexander.Getmansky <getmansk-y@yandex.ru>
2013-12-09 16:59:02 +0400

    Added test for 'get' and 'set' services

--- tests/GetSky/Phalcon/AutoloadServices/Tests/Creators/CreatorTest.php
+++ tests/GetSky/Phalcon/AutoloadServices/Tests/Creators/CreatorTest.php
@@ -1,32 +1,39 @@
 <?php
 namespace GetSky\Phalcon\AutoloadServices\Tests\Creators;
 
+use GetSky\Phalcon\AutoloadServices\Creators\AbstractCreator;
 use Phalcon\Config;
 use Phalcon\DI\FactoryDefault;
 use Phalcon\DiInterface;
 use PHPUnit_Framework_TestCase;
-use \GetSky\Phalcon\AutoloadServices\Creators\AbstractCreator;
 
 /**
  * Class CreatorTest
  * @package GetSky\Phalcon\AutoloadServices\Tests\Creators
  */
-abstract class CreatorTest extends PHPUnit_Framework_TestCase {
+abstract class CreatorTest extends PHPUnit_Framework_TestCase
+{
 
     /**
      * @var AbstractCreator
      */
     protected $creator = null;
-
     /**
      * @var Config
      */
     protected $services = null;
-
     /**
      * @var DiInterface
      */
     protected $di = null;
+
+    public function testSetAndGetServices()
+    {
+        $save = $this->services;
+        $this->creator->setService(new Config(array('test' => 'test')));
+        $this->assertArrayHasKey('test',$this->creator->getService());
+        $this->services = $save;
+    }
 
     protected function setUp()
     {
