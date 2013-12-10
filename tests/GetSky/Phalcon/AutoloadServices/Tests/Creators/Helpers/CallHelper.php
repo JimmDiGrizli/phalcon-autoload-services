782bdc75bc65e5b00e2bf9e04f45914a5ce92a78
Alexander.Getmansky <getmansk-y@yandex.ru>
2013-12-10 12:09:01 +0400

    Added blank tests for helpers

---
+++ tests/GetSky/Phalcon/AutoloadServices/Tests/Creators/Helpers/CallHelper.php
@@ -0,0 +1,10 @@
+<?php
+namespace GetSky\Phalcon\AutoloadServices\Tests\Creators\Helpers;
+
+class CallHelper extends HelperTest {
+
+    public function  testPreparation()
+    {
+
+    }
+}
---
+++ tests/GetSky/Phalcon/AutoloadServices/Tests/Creators/Helpers/HelperTest.php
@@ -0,0 +1,40 @@
+<?php
+namespace GetSky\Phalcon\AutoloadServices\Tests\Creators\Helpers;
+
+use Phalcon\Config;
+use Phalcon\DI\FactoryDefault;
+use Phalcon\DiInterface;
+use PHPUnit_Framework_TestCase;
+use GetSky\Phalcon\AutoloadServices\Creators\Helpers\AbstractHelper;
+
+/**
+ * Class HelperTest
+ * @package GetSky\Phalcon\AutoloadServices\Tests\Creators\Helpers
+ */
+abstract class HelperTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var AbstractHelper
+     */
+    protected $creator = null;
+    /**
+     * @var Config
+     */
+    protected $services = null;
+    /**
+     * @var DiInterface
+     */
+    protected $di = null;
+
+    protected function setUp()
+    {
+        $this->di = new FactoryDefault();
+    }
+
+    protected function tearDown()
+    {
+        $this->di = null;
+    }
+
+    abstract public function testPreparation();
+}
