6d46b155c621c5ba377807cfdd732da3a953b943
Alexander.Getmansky <getmansk-y@yandex.ru>
2013-12-12 12:12:15 +0400

    Added test for ArgumentsHelper

--- tests/GetSky/Phalcon/AutoloadServices/Tests/Creators/Helpers/ArgumentsHelperTest.php
+++ tests/GetSky/Phalcon/AutoloadServices/Tests/Creators/Helpers/ArgumentsHelperTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace GetSky\Phalcon\AutoloadServices\Tests\Creators\Helpers;
 
+use CallService;
 use GetSky\Phalcon\AutoloadServices\Creators\Helpers\ArgumentsHelper;
 use Phalcon\Config;
 
@@ -13,7 +14,22 @@ class ArgumentsHelperTest extends HelperTest
 
     public function  testPreparation()
     {
+        $params = [
+            '0' => 24,
+            '1' => 21,
+            '2' => $this->di,
+            '3' => new CallService(),
+            '4' => new CallService(),
+            '5' => new CallService(),
+            '6' => $this->di->get('tag'),
+            '7' => $this->di->getShared('tag'),
+            '8' => $this->di->getShared('tag')
+        ];
+        $this->assertEquals($params, $this->helper->preparation());
 
+        $config = new Config(['0' => new Config(['var' => '%off%'])]);
+        $this->helper->setConfig($config);
+        $this->assertNull($this->helper->preparation());
     }
 
     /**
@@ -22,7 +38,24 @@ class ArgumentsHelperTest extends HelperTest
     public function testBadArgumentsException()
     {
         $config = new Config(
-            array('test' => new Config(array('test' => 'test')))
+            ['test' => new Config(['test' => 'test'])]
+        );
+        $this->helper->setConfig($config);
+        $this->helper->preparation();
+        $this->helper->setConfig($this->config);
+    }
+
+    /**
+     * @expectedException \GetSky\Phalcon\AutoloadServices\Creators\Exception\MissClassNameException
+     */
+    public function testMissClassNameExceptionException()
+    {
+        $config = new Config(
+            [
+                '0' => new Config(
+                        ['object' => new Config(['fail' => 'CallService'])]
+                    )
+            ]
         );
         $this->helper->setConfig($config);
         $this->helper->preparation();
@@ -33,7 +66,23 @@ class ArgumentsHelperTest extends HelperTest
     {
         parent::setUp();
         $this->config = new Config(
-            array('0' => new Config(array('var' => '24')))
+            [
+                '0' => new Config(['var' => '24']),
+                '1' => new Config(['parameter' => '21']),
+                '2' => new Config(['di' => 1]),
+                '3' => new Config(
+                        ['object' => new Config(['obj' => 'CallService'])]
+                    ),
+                '4' => new Config(
+                        ['obj' => new Config(['instance' => 'CallService'])]
+                    ),
+                '5' => new Config(
+                        ['instance' => new Config(['object' => 'CallService'])]
+                    ),
+                '6' => new Config(['service' => 'tag']),
+                '7' => new Config(['shared-service' => 'tag']),
+                '8' => new Config(['s-service' => 'tag']),
+            ]
         );
         $this->helper = new ArgumentsHelper($this->di, $this->config);
     }
